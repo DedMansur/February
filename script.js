e60f97c25d3c194f8292afb516a0c14caf6370f5
Parviz <sosiskakiller6482@gmail.com>
2024-02-13 21:16:01 +0500

    script

---
+++ script.js
@@ -0,0 +1,30 @@
+const heart = document.getElementById('heart');
+    const modal = document.getElementById('myModal');
+    const closeBtn = document.getElementsByClassName('close')[0];
+
+    window.addEventListener('DOMContentLoaded', () => {
+        const audio = document.getElementById('myAudio');
+        audio.play();
+    });
+
+    heart.addEventListener('click', () => {
+        heart.style.color = 'blue'; 
+        document.body.style.backgroundColor = '#fce0d9'; 
+        modal.style.display = 'block'; 
+    });
+
+    closeBtn.addEventListener('click', () => {
+        modal.style.animationName = 'fadeOut'; 
+        setTimeout(() => {
+            modal.style.display = 'none'; 
+            modal.style.animationName = ''; 
+        }, 500); 
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            closeBtn.click(); 
+        }
+        
+    });
+
